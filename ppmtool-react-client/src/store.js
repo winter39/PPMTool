8f5c5d8659fbdd9aa7d65ca139dc36aee3d0136c
winter39 <70872266+winter39@users.noreply.github.com>
2021-08-04 17:06:35 -0400

    first commit

---
+++ ppmtool-react-client/src/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initalState = {};
+const middleware = [thunk];
+
+let store;
+
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// work in charome and other browsers
+if (window.navigator.userAgent.includes("Chrome")&& ReactReduxDevTools) {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(
+      applyMiddleware(...middleware),
+      ReactReduxDevTools
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(applyMiddleware(...middleware))
+  );
+}
+
+export default store;
